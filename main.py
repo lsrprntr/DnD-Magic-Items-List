1824b7eb091a2c53efcf03158d529a420585448a
sssst <lsrprntr@gmail.com>
2024-03-25 18:06:23 +1100

    c

--- main.py
+++ main.py
@@ -1,60 +1,83 @@
-from bs4 import BeautifulSoup
 import urllib.request, urllib.parse, urllib.error
+import json
+from bs4 import BeautifulSoup
 
-mainLink = "https://www.dndbeyond.com/magic-items"
-pagesLink = "https://www.dndbeyond.com/magic-items?page="
+MAIN_LINK = "https://www.dndbeyond.com/magic-items?filter-partnered-content="
+PAGES_LINK = "https://www.dndbeyond.com/magic-items?filter-partnered-content=&page="
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+HEADERS = {'User-Agent': USER_AGENT, "Accept-Language": "en-US, en;q=0.5"}
+SEARCH_FILTERS = {"class": "b-pagination-list paging-list j-tablesorter-pager j-listing-pagination"}
 
-user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
-headers = {'User-Agent': user_agent, "Accept-Language": "en-US, en;q=0.5"}
-
-search_class = {"class": "b-pagination-list paging-list j-tablesorter-pager j-listing-pagination"}
-
+MAGIC_ITEMS = []
+# Dict of magic items
+# Name/id
+#   Rarity
+#   Type
+#   Attunement
+#   Source Book
 
 def main():
-    soup = fetchPage(mainLink)
-    
-    #Get page numbers
-    page_numbers_list = soup.find("ul", search_class).find_all("a", {"class":"b-pagination-item"})
-    lastPage = page_numbers_list[-1].getText()
-    
-    #Main loop
-    
-    pageParse(1)
-    """for _ in range(2,lastPage):
-        #something append list
-        print("yes")"""
-    return
+    soup = fetchPage(MAIN_LINK)
 
-def pageParse(pageNumber:int):
-    link = pagesLink + str(pageNumber)
-    
-    #Try request
-    soup = fetchPage(link)
-    
-    result = soup.find_all("div", {"class": "info", "data-type": "magic-items"})
-    print(result[0].find("a"))
-    
-    return
+    # Get page numbers
+    page_numbers_list = soup.find("ul", SEARCH_FILTERS).find_all("a", {"class": "b-pagination-item"})
+    lastPage = int(page_numbers_list[-1].getText())
+
+    # Main loop
+    if lastPage > 1:
+        for pageNumber in range(1, lastPage+1):
+            print(f"Parsing page: {pageNumber}")
+            pageParse(pageNumber)
+            break
+    else:
+        pageParse(1)
+
+    with open(file='magic_items.json', mode='w', encoding="utf-8") as file:
+        json.dump(MAGIC_ITEMS, file)
 
 
-def fetchPage(link:str):
-    response = urllib.request.Request(link, headers=headers)
+
+def pageParse(pageNumber: int):
+    link = PAGES_LINK + str(pageNumber)
+    test = "https://www.dndbeyond.com/magic-items?filter-type=0&filter-search=far+sight&filter-requires-attunement=&filter-effect-type=&filter-effect-subtype=&filter-has-charges=&filter-partnered-content="
+    # Try request
+    soup = fetchPage(test)
+
+    # The List
+    itemList = soup.find_all("div", {"class": "info", "data-type": "magic-items"})
+
+    # The Split Parse
+    for item in itemList:
+        href = item.find_all("a", {"class": "link"})[-1]["href"].split("/magic-items/")[-1]
+        print(href)
+        itemID = href.split("-")[0]
+        name = item.find("span", {"class": "name"}).get_text().strip().lstrip("Partnered Content").rstrip("\n\n\n\n").strip()
+        print(name)
+        rarity = item.find("span", {"class": "rarity"}).get_text().strip()
+        itemType = item.find("span", {"class": "type"}).get_text().strip()
+
+        # Building the item
+        magic_item = {}
+        magic_item["id"] = itemID
+        magic_item["name"] = name
+        magic_item["rarity"] = rarity
+        magic_item["type"] = itemType
+
+        MAGIC_ITEMS.append(magic_item)
+        print(f"Added: {name}")
+
+
+
+def fetchPage(link: str):
+    response = urllib.request.Request(link, headers=HEADERS)
     try:
         page = urllib.request.urlopen(response)
     except:
         print("Error: Requesting Page Failed: " + link)
-        raise Exception()
+        raise
+
     return BeautifulSoup(page, 'html.parser')
-    
-    
+
+
 if __name__ == "__main__":
     main()
-    
-    
-[]
-#List of magic items
-#Name
-#Rarity
-#Type
-#Attunement
-#Source Book
---
+++ test.py
@@ -0,0 +1,2 @@
+text = "Shield of Far Sight\n\n\n\n        Legacy\n    \n\nThis doesn't reflect the latest rules and lore.\nLearn More"
+print(text.split("\n\n\n\n"))
