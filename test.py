e653e4eec41c74eb2fb3a139a3ba62f0398c9c32
sssst <lsrprntr@gmail.com>
2024-03-26 12:36:36 +1100

    t

--- test.py
+++ test.py
@@ -8,14 +8,12 @@ from main import fetchPage, pageParse
 TEST_LINKS = [
     "https://www.dndbeyond.com/magic-items?filter-type=0&filter-search=Dragonguard+&filter-requires-attunement=&filter-effect-type=&filter-effect-subtype=&filter-has-charges=&filter-partnered-content=f",
     "https://www.dndbeyond.com/magic-items?filter-type=0&filter-search=Agony&filter-requires-attunement=&filter-effect-type=&filter-effect-subtype=&filter-has-charges=&filter-partnered-content=t",
-    
 ]
 
 def test():
     for link in TEST_LINKS:
         soup = fetchPage(link)
-        
-    
+
 
 
 print(page.status_code)
